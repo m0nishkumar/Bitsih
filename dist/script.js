9806db566b8fd334ef3d24295d96a1560961dd7f
Theternos <kavin.apm2003@gmail.com>
2022-10-12 14:45:41 +0530

    dist deleted

--- dist/script.js
+++
@@ -1,5 +0,0 @@
-// The original responsive design is taken from Geoff Yuen here: https://codepen.io/geoffyuen/pen/FCBEg
-
-// Geoff Yuen gave credit as follows: Thanks to @leefroese for suggestion about data attributes
-
-// I loved his mobile design, but changed some of the styling to match up with what I wanted. I'm also not using SCSS or anything else; just HTML and CSS.
